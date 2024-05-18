3cb790dd1bfe4c1ca0b8324c86aa71bf572c3b4c
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-05-18 11:51:35 +0200

    admin change

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Admin = () => {
   return (
     <div>
-      <form className="grid gap-3">
+      <form className="grid gap-3 p-14 w-96">
         <input
-          className="border-1 border-black border-solid"
+          className="border-2 border-black border-solid"
           type="text"
           placeholder="add title"
         />
         <input
-          className="border-1 border-black border-solid"
+          className="border-2 border-black border-solid"
           type="text"
           placeholder="description"
         />
